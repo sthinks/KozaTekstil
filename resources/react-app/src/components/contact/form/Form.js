dc107ed1143436e4fc012521604c208147b91dfd
Onur Çöpoğlu <100149612+onurcpgl@users.noreply.github.com>
2023-01-26 18:30:45 +0300

    Contact

--- resources/react-app/src/components/contact/form/Form.js
+++ resources/react-app/src/components/contact/form/Form.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import contactImage from '../../../assets/contact/contact-form-image.png'
 function Form() {
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
+      name: '',
+      surname: '',
+      message: '',
     },
+    validationSchema: Yup.object({
+      name: Yup.string().min(2).required('Zorunlu alan'),
+      surname: Yup.string().min(2).required('Zorunlu alan'),
+      message: Yup.string().required('Zorunlu alan'),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2))
     },
@@ -29,48 +35,68 @@ function Form() {
           </div>
           <div className="form">
             <div className="w-[576px] h-[576px] bg-[#FFFFFF] shadow shadow-slate-300 rounded-4xl">
-              <div className="">
-                <p className="px-10  py-2 mb-3 ">Get In Touch</p>
+              <div className="p-6">
+                <div className="flex justify-start px-12">
+                  <p className="py-5 mb-3 text-xl font-semibold">
+                    Get In Touch
+                  </p>
+                </div>
                 <div className="form-content">
                   <form
-                    className="flex justify-center items-center flex-col"
+                    className="flex justify-center items-center flex-col relative"
                     onSubmit={formik.handleSubmit}
                   >
                     <input
                       className="bg-[#F2F1F1] w-10/12 h-12 rounded-4xl px-10  py-2 mb-3 outline-none"
-                      id="firstName"
-                      name="firstName"
+                      id="name"
+                      name="name"
                       type="text"
                       placeholder="Name"
                       onChange={formik.handleChange}
-                      value={formik.values.firstName}
+                      value={formik.values.name}
                     />
-
+                    {formik.errors.name && (
+                      <p className="absolute top-[-35px] right-0 p-2 rounded-form-error bg-slate-400">
+                        {formik.errors.name}
+                      </p>
+                    )}
                     <input
                       className="bg-[#F2F1F1] w-10/12 h-12 rounded-4xl px-10  py-2 mb-3 outline-none"
-                      id="lastName"
-                      name="lastName"
+                      id="surname"
+                      name="surname"
                       type="text"
                       placeholder="Surname"
                       onChange={formik.handleChange}
-                      value={formik.values.lastName}
+                      value={formik.values.surname}
                     />
+                    {formik.errors.surname && (
+                      <p className="absolute top-[40px] right-0 p-2 rounded-form-error bg-slate-400">
+                        {formik.errors.surname}
+                      </p>
+                    )}
 
                     <textarea
                       className="bg-[#F2F1F1] w-10/12 h-60 rounded-4xl px-10  py-2 mb-3 outline-none"
-                      id="email"
-                      name="email"
-                      type="email"
+                      id="message"
+                      name="message"
+                      type="message"
                       placeholder="Type Your Message"
                       onChange={formik.handleChange}
-                      value={formik.values.email}
+                      value={formik.values.message}
                     />
-                    <button
-                      className="w-1/4 h-12 bg-[#F2F1F1] rounded-4xl"
-                      type="submit"
-                    >
-                      Send
-                    </button>
+                    {formik.errors.message && (
+                      <p className="absolute top-[120px] right-0 p-2 rounded-form-error bg-slate-400">
+                        {formik.errors.message}
+                      </p>
+                    )}
+                    <div className="flex justify-end w-full px-10">
+                      <button
+                        className="w-2/6 h-12 bg-[#F2F1F1] rounded-4xl hover:bg-[#E9E9E9] hover:shadow-sm"
+                        type="submit"
+                      >
+                        Send
+                      </button>
+                    </div>
                   </form>
                 </div>
               </div>
