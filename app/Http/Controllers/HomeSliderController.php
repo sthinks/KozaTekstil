f5e58fb090c217818d5a4d0a0853c72987030b28
Onur Çöpoğlu <100149612+onurcpgl@users.noreply.github.com>
2023-02-05 16:37:53 +0300

    Home slider api

--- app/Http/Controllers/HomeSliderController.php
+++ app/Http/Controllers/HomeSliderController.php
@@ -16,10 +16,34 @@ class HomeSliderController extends Controller
     {
         $data = Slider::all();
         $data->map(function ($item) {
-            $item->image = url(
-                sprintf('storage/%s', str_replace('\\', '/', $item->image))
-            );
+            if ($item->image) {
+                $item->image = url(
+                    sprintf('storage/%s', str_replace('\\', '/', $item->image))
+                );
+            } else {
+                $result = null;
+                $item->image = $result;
+                return $item->image;
+            }
         });
+        $data->map(function ($item) {
+            $video = $item->video;
+            if (!empty($video)) {
+                $array = json_decode($item->video, true);
+                $item->video = array_map(function ($video) {
+                    $video['download_link'] =
+                        url('storage/') .
+                        '/' .
+                        str_replace('\\', '/', $video['download_link']);
+                    return $video;
+                }, $array);
+            } else {
+                $result = null;
+                $item->video = $result;
+                return $item->video;
+            }
+        });
+
         return response()->json($data);
     }
 }
